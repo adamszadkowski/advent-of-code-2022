e731b8b04fee4ce18e2715c9a3f3993a6dcb170f
Adam Szadkowski <adam@szadkowski.info>
2022-12-03 10:12:08 +0100

    Solve day 3 part 2

--- day-3 part-2.js
+++ day-3 part-2.js
@@ -6,21 +6,20 @@
         return r.responseText;
     }
 
-    const splitInHalf = (s) => {
-        const halfIndex = s.length / 2;
-        return [
-            s.substring(0, halfIndex),
-            s.substring(halfIndex),
-        ];
-    };
+    const chunk = (array, size) =>
+        array.reduce((acc, _, i) => {
+            if (i % size === 0) acc.push(array.slice(i, i + size))
+            return acc
+        }, []);
 
-    const findCommonItem = (i1, i2) => {
+    const findCommonItems = (i1, i2, i3) => {
         const arr1 = i1.split("");
         const arr2 = i2.split("");
+        const arr3 = i3.split("");
 
-        const intersect = (a1, a2) => a1.find(i => a2.includes(i));
+        const intersect = (a1, a2, a3) => a1.find(i => a2.includes(i) && a3.includes(i));
 
-        return intersect(arr1, arr2);
+        return intersect(arr1, arr2, arr3);
     };
 
     const toPoints = (e) => {
@@ -32,11 +31,8 @@
         throw new Error("Incorrect");
     };
 
-    return httpGet("https://adventofcode.com/2022/day/3/input")
-        .split("\n")
-        .filter(s => s)
-        .map(s => splitInHalf(s))
-        .map(([i1, i2]) => findCommonItem(i1, i2))
+    return chunk(httpGet("https://adventofcode.com/2022/day/3/input").split("\n").filter(s => s), 3)
+        .map(([i1, i2, i3]) => findCommonItems(i1, i2, i3))
         .map(e => toPoints(e))
         .reduce((a, c) => a + c);
 })();
