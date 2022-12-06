f18570afff4e4647feeb623982c99294962db583
Adam Szadkowski <adam@szadkowski.info>
2022-12-06 09:57:11 +0100

    Solve day 6 part 1

---
+++ day-6.js
@@ -0,0 +1,25 @@
+(() => {
+    const httpGet = (url) => {
+        var r = new XMLHttpRequest();
+        r.open("GET", url, false);
+        r.send(null);
+        return r.responseText;
+    }
+
+    const findBeggining = (buffer) => {
+        const marker = [];
+        for (let i = 0; i < buffer.length; i++) {
+            const l = buffer[i];
+            if (marker.length === 4) {
+                if (new Set(marker).size === 4) {
+                    return i;
+                } else {
+                    marker.shift();
+                }
+            }
+            marker.push(l);
+        }
+    };
+
+    return findBeggining(httpGet("https://adventofcode.com/2022/day/6/input"));
+})();
