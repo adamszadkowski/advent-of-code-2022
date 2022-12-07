506b338238a330421dc09b685af45c342f5f8df7
Adam Szadkowski <adam@szadkowski.info>
2022-12-07 20:15:28 +0100

    Extract commands

--- src/day-7/__tests__/day-7.spec.js
+++ src/day-7/__tests__/day-7.spec.js
@@ -1,5 +1,55 @@
 describe("day 6", () => {
-    it("does nothing", () => {
-        expect("").toBe("");
+    let solution;
+
+    beforeEach(() => {
+        solution = new Day7Solution();
+    });
+
+    it("extracts single command without output", () => {
+        const input = "cd /";
+
+        expect(solution.extractCommand(input)).toEqual(
+            { command: "cd", argument: "/", output: [] },
+        );
+    });
+
+    it("extracts single command with output", () => {
+        const input = `ls
+1 file.txt`;
+
+        expect(solution.extractCommand(input)).toEqual(
+            { command: "ls", argument: null, output: ["1 file.txt"] },
+        );
+    });
+
+    it("extracts multiple commands", () => {
+        const input = `$ cd /
+$ ls
+1 file.txt`;
+
+        expect(solution.extractCommands(input)).toEqual([
+            { command: "cd", argument: "/", output: [] },
+            { command: "ls", argument: null, output: ["1 file.txt"] },
+        ]);
     });
 });
+
+class Day7Solution {
+    extractCommands(input) {
+        const [, ...commands] = input.split("$ ");
+
+        return commands
+            .map((c) => {
+                const sanitized = c.endsWith("\n") ? c.slice(0, -1) : c;
+                return this.extractCommand(sanitized);
+            });
+    }
+
+    extractCommand(input) {
+        const [commandLine, ...content] = input.split("\n");
+        const [command, argument] = commandLine.split(" ");
+        const output = content || [];
+
+        return { command: command, argument: argument || null, output: output };
+    }
+};
