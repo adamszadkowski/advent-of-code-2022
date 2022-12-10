9f36be02df3a80975abfa900d15f4d0c55f043da
Adam Szadkowski <adam@szadkowski.info>
2022-12-10 11:49:32 +0100

    Extract part 1 test suite

--- src/day-9/__tests__/part-1.spec.js
+++ src/day-9/__tests__/part-1.spec.js
@@ -2,94 +2,95 @@ import { describe, beforeEach, test, expect } from "@jest/globals";
 import { Day9Solution } from "@/day-9/part-1";
 
 describe("day 9", () => {
-    let solver;
+    describe("part 1", () => {
+        let solver;
 
-    beforeEach(() => {
-        solver = new Day9Solution();
-    });
-
-    [
-        { input: "U 1", result: ["U"] },
-        { input: "L 1", result: ["L"] },
-        { input: "D 1", result: ["D"] },
-        { input: "R 1", result: ["R"] },
-        { input: "U 3", result: ["U", "U", "U"] },
-    ].forEach(({ input, result }) => {
-        test(`decode move ${input}`, () => {
-            expect(solver.decode(input)).toEqual(result);
+        beforeEach(() => {
+            solver = new Day9Solution();
         });
-    });
 
-    test("decode moves", () => {
-        const input = `U 1
+        [
+            { input: "U 1", result: ["U"] },
+            { input: "L 1", result: ["L"] },
+            { input: "D 1", result: ["D"] },
+            { input: "R 1", result: ["R"] },
+            { input: "U 3", result: ["U", "U", "U"] },
+        ].forEach(({ input, result }) => {
+            test(`decode move ${input}`, () => {
+                expect(solver.decode(input)).toEqual(result);
+            });
+        });
+
+        test("decode moves", () => {
+            const input = `U 1
                        D 2
                        L 1
                        R 3`.replace(/\n +/g, "\n");
 
-        expect(solver.decode(input)).toEqual(["U", "D", "D", "L", "R", "R", "R"]);
-    });
+            expect(solver.decode(input)).toEqual(["U", "D", "D", "L", "R", "R", "R"]);
+        });
 
-    test("track head moves", () => {
-        const input = `U 1
+        test("track head moves", () => {
+            const input = `U 1
                        L 1
                        D 1
                        R 1`.replace(/\n +/g, "\n");
-        expect(solver.move(input).head).toEqual([
-            { x: 0, y: 0 },
-            { x: 1, y: 0 },
-            { x: 1, y: -1 },
-            { x: 0, y: -1 },
-            { x: 0, y: 0 },
-        ]);
-    });
+            expect(solver.move(input).head).toEqual([
+                { x: 0, y: 0 },
+                { x: 1, y: 0 },
+                { x: 1, y: -1 },
+                { x: 0, y: -1 },
+                { x: 0, y: 0 },
+            ]);
+        });
 
-    test("track tail moves", () => {
-        const input = `U 2`.replace(/\n +/g, "\n");
-        expect(solver.move(input).tail).toEqual([
-            { x: 0, y: 0 },
-            { x: 1, y: 0 },
-        ]);
-    });
+        test("track tail moves", () => {
+            const input = `U 2`.replace(/\n +/g, "\n");
+            expect(solver.move(input).tail).toEqual([
+                { x: 0, y: 0 },
+                { x: 1, y: 0 },
+            ]);
+        });
 
-    test("hide tail under head", () => {
-        const input = `U 1
+        test("hide tail under head", () => {
+            const input = `U 1
                        D 2
                        U 1`.replace(/\n +/g, "\n");
-        expect(solver.move(input).tail).toEqual([
-            { x: 0, y: 0 },
-        ]);
-    });
+            expect(solver.move(input).tail).toEqual([
+                { x: 0, y: 0 },
+            ]);
+        });
 
-    test("move tail on corners", () => {
-        const input = `R 1
+        test("move tail on corners", () => {
+            const input = `R 1
                        U 2`.replace(/\n +/g, "\n");
-        expect(solver.move(input).tail).toEqual([
-            { x: 0, y: 0 },
-            { x: 1, y: 1 },
-        ]);
-    });
+            expect(solver.move(input).tail).toEqual([
+                { x: 0, y: 0 },
+                { x: 1, y: 1 },
+            ]);
+        });
 
-    test("move tail on corners with get back", () => {
-        const input = `R 1
+        test("move tail on corners with get back", () => {
+            const input = `R 1
                        U 1
                        L 1`.replace(/\n +/g, "\n");
-        expect(solver.move(input).tail).toEqual([
-            { x: 0, y: 0 },
-        ]);
-    });
+            expect(solver.move(input).tail).toEqual([
+                { x: 0, y: 0 },
+            ]);
+        });
 
-    test("move tail on two corners", () => {
-        const input = `R 1
+        test("move tail on two corners", () => {
+            const input = `R 1
                        U 1
                        R 1`.replace(/\n +/g, "\n");
-        expect(solver.move(input).tail).toEqual([
-            { x: 0, y: 0 },
-            { x: 1, y: 1 },
-        ]);
-    });
+            expect(solver.move(input).tail).toEqual([
+                { x: 0, y: 0 },
+                { x: 1, y: 1 },
+            ]);
+        });
 
-    test("count distinct moves", () => {
-        const input = `R 4
+        test("count distinct moves", () => {
+            const input = `R 4
                        U 4
                        L 3
                        D 1
@@ -97,6 +98,7 @@ describe("day 9", () => {
                        D 1
                        L 5
                        R 2`.replace(/\n +/g, "\n");
-        expect(solver.countDistinctTailMoves(input)).toEqual(13);
+            expect(solver.countDistinctTailMoves(input)).toEqual(13);
+        });
     });
 });
