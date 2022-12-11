87e8204264564ee728a9c2abbbeb0d2742cd41da
Adam Szadkowski <adam@szadkowski.info>
2022-12-11 15:13:33 +0100

    Cycle commands

--- src/day-10/__tests__/solution.spec.js
+++ src/day-10/__tests__/solution.spec.js
@@ -16,4 +16,43 @@ describe("day 10", () => {
             { command: "addx", x: 1 },
         ])
     });
+
+    test("cycle noop command", () => {
+        const input = `noop`;
+        expect(solution.cycle(input)).toEqual([
+            { cycle: 1, command: "noop", x: 1 },
+        ]);
+    });
+
+    test("cycle noop command twice", () => {
+        const input = `noop
+                       noop`.replace(/\n +/g, "\n");
+        expect(solution.cycle(input)).toEqual([
+            { cycle: 1, command: "noop", x: 1 },
+            { cycle: 2, command: "noop", x: 1 },
+        ]);
+    });
+
+    test("cycle addx command", () => {
+        const input = `addx 2`;
+        expect(solution.cycle(input)).toEqual([
+            { cycle: 1, command: "addx", x: 1 },
+            { cycle: 2, command: "addx", x: 1 },
+        ]);
+    });
+
+    test("cycle sample commands", () => {
+        const input = `noop
+                       addx 3
+                       addx -5
+                       noop`.replace(/\n +/g, "\n");
+        expect(solution.cycle(input)).toEqual([
+            { cycle: 1, command: "noop", x: 1 },
+            { cycle: 2, command: "addx", x: 1 },
+            { cycle: 3, command: "addx", x: 1 },
+            { cycle: 4, command: "addx", x: 4 },
+            { cycle: 5, command: "addx", x: 4 },
+            { cycle: 6, command: "noop", x: -1 },
+        ]);
+    });
 });
--- src/day-10/solution.js
+++ src/day-10/solution.js
@@ -1,4 +1,25 @@
 export class Day10Solution {
+    cycle(input) {
+        const commands = this.load(input);
+        const cycles = [];
+        let counter = 1;
+        let x = 1;
+
+        while (commands.length > 0) {
+            const command = commands.shift();
+            if (command.command === "noop") {
+                cycles.push({ cycle: counter, command: command.command, x });
+            } else if (command.command === "addx") {
+                cycles.push({ cycle: counter++, command: command.command, x });
+                cycles.push({ cycle: counter, command: command.command, x });
+                x += command.x;
+            }
+            counter++;
+        }
+
+        return cycles;
+    }
+
     load(input) {
         return input.split("\n")
             .filter(l => l)
