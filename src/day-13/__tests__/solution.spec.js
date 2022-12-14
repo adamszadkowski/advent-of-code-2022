60ebedc2fc05e4de211bb8c76cad0e6a6a9d8c14
Adam Szadkowski <adam@szadkowski.info>
2022-12-14 20:40:14 +0100

    Compare single element arrays

--- src/day-13/__tests__/solution.spec.js
+++ src/day-13/__tests__/solution.spec.js
@@ -13,6 +13,16 @@ describe("day 13", () => {
             second: 0,
             isCorrect: false,
         },
+        {
+            first: [1],
+            second: 2,
+            isCorrect: true,
+        },
+        {
+            first: 2,
+            second: [1],
+            isCorrect: false,
+        },
     ].forEach(({ first, second, isCorrect }) => {
         test(`messages ${first} and ${second} are in order: ${isCorrect}`, () => {
             expect(compare(first, second)).toBe(isCorrect);
