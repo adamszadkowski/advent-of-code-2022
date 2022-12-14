51d53cec3cea532a83fb822c3f6cf8d9aa27ab79
Adam Szadkowski <adam@szadkowski.info>
2022-12-14 20:35:04 +0100

    Compare two integers

--- src/day-13/__tests__/solution.spec.js
+++ src/day-13/__tests__/solution.spec.js
@@ -1,7 +1,25 @@
 import { describe, expect, test } from "@jest/globals";
 
 describe("day 13", () => {
-    test("do nothing", () => {
-        expect(1).toBe(1);
+
+    [
+        {
+            first: 0,
+            second: 1,
+            isCorrect: true,
+        },
+        {
+            first: 1,
+            second: 0,
+            isCorrect: false,
+        },
+    ].forEach(({ first, second, isCorrect }) => {
+        test(`messages ${first} and ${second} are in order: ${isCorrect}`, () => {
+            expect(compare(first, second)).toBe(isCorrect);
+        });
     });
 });
+
+function compare(first, second) {
+    return first < second;
+}
