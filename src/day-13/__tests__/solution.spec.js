d6b16e5a0dee54b7360c604bc52f477d008f6ac8
Adam Szadkowski <adam@szadkowski.info>
2022-12-14 21:13:39 +0100

    Add more tests from sample

--- src/day-13/__tests__/solution.spec.js
+++ src/day-13/__tests__/solution.spec.js
@@ -20,8 +20,32 @@ describe("day 13", () => {
             second: [1, 2],
         },
         {
+            first: [1, 1, 3, 1, 1],
+            second: [1, 1, 5, 1, 1],
+        },
+        {
+            first: [[1], [2, 3, 4]],
+            second: [[1], 4],
+        },
+        {
+            first: [[8, 7, 6]],
+            second: [9],
+        },
+        {
+            first: [[4, 4], 4, 4],
+            second: [[4, 4], 4, 4, 4],
+        },
+        {
+            first: [7, 7, 7],
+            second: [7, 7, 7, 7],
+        },
+        {
             first: [],
             second: [3],
+        },
+        {
+            first: [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+            second: [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
         },
     ].forEach(({ first, second, isCorrect }) => {
         test(`messages ${first} and ${second} are in order: ${isCorrect}`, () => {
