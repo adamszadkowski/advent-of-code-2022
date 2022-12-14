f52ac5026f88fd6a9eae3ecebb9c0ad874738588
Adam Szadkowski <adam@szadkowski.info>
2022-12-14 21:08:19 +0100

    Simplify tests

--- src/day-13/__tests__/solution.spec.js
+++ src/day-13/__tests__/solution.spec.js
@@ -6,56 +6,27 @@ describe("day 13", () => {
         {
             first: 0,
             second: 1,
-            isCorrect: true,
-        },
-        {
-            first: 1,
-            second: 0,
-            isCorrect: false,
         },
         {
             first: [1],
             second: 2,
-            isCorrect: true,
-        },
-        {
-            first: 2,
-            second: [1],
-            isCorrect: false,
         },
         {
             first: [1],
             second: [2],
-            isCorrect: true,
-        },
-        {
-            first: [2],
-            second: [1],
-            isCorrect: false,
         },
         {
             first: [1, 1],
             second: [1, 2],
-            isCorrect: true,
-        },
-        {
-            first: [1, 2],
-            second: [1, 1],
-            isCorrect: false,
         },
         {
             first: [],
             second: [3],
-            isCorrect: true,
-        },
-        {
-            first: [3],
-            second: [],
-            isCorrect: false,
         },
     ].forEach(({ first, second, isCorrect }) => {
         test(`messages ${first} and ${second} are in order: ${isCorrect}`, () => {
-            expect(compare(first, second)).toBe(isCorrect);
+            expect(compare(first, second)).toBeTruthy();
+            expect(compare(second, first)).toBeFalsy();
         });
     });
 });
