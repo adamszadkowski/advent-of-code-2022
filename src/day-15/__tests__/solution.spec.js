c17cbe8d4eff24e31013def4558fc6a52a18de04
Adam Szadkowski <adam@szadkowski.info>
2022-12-17 11:07:54 +0100

    Visualize two sensors

--- src/day-15/__tests__/solution.spec.js
+++ src/day-15/__tests__/solution.spec.js
@@ -1,7 +1,45 @@
 import { describe, expect, test } from "@jest/globals";
 
 describe("day 15", () => {
-    test("do nothing", () => {
-        expect(1).toBe(1);
+    test("create map", () => {
+        const map = createMap();
+
+        map.addSensor(2, 18);
+        map.addSensor(9, 16);
+
+        expect(map.visualize()).toEqual(
+            `.......S
+             ........
+             S.......`.replace(/ +/g, "")
+        )
     });
 });
+
+function createMap() {
+    const map = [];
+    let min;
+    let max;
+
+    const updateBoundaries = (x, y) => {
+        min = { x: Math.min(min?.x ?? x, x), y: Math.min(min?.y ?? y, y) };
+        max = { x: Math.max(max?.x ?? x, x), y: Math.max(max?.y ?? y, y) };
+    };
+
+    return {
+        addSensor(x, y) {
+            updateBoundaries(x, y);
+            (map[y] || (map[y] = []))[x] = "S";
+        },
+        visualize() {
+            let result = "";
+            for (let y = min.y; y <= max.y; y++) {
+                let row = "";
+                for (let x = min.x; x <= max.x; x++) {
+                    row += map[y]?.[x] || ".";
+                }
+                result += row + (y != max.y ? "\n" : "");
+            }
+            return result;
+        },
+    };
+}
